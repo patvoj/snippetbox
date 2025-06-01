b84b8bcfe9fdb14b8e369ba81d41a08fc831abff
Patrik Vojta <vojta.patrik@gmail.com>
2025-06-01 10:18:55 +0200

    Complete logging

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,17 +1,40 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	ui "github.com/patvoj/snippetbox/ui/html/pages"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	
 	err := ui.Home().Render(r.Context(), w)
 	if err != nil {
-		log.Printf("error rendering home: %v", err)
+		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
+}
+
+func (app *application) getSnippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
+}
+
+func (app *application) getSnippetForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a snippet..."))
+}
+
+func (app *application) postSnippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Save a new snippet..."))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,32 +2,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
-	"strconv"
 )
-
-func getSnippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-	w.Write([]byte(msg))
-}
-
-func getSnippetForm(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create a snippet..."))
-}
-
-func postSnippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Save a new snippet..."))
-}
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
@@ -38,19 +16,27 @@ func main() {
 		Level:     slog.LevelDebug,
 	}))
 
+	app := application{
+		logger: logger,
+	}
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", getSnippetView)
-	mux.HandleFunc("GET /snippet/create", getSnippetForm)
-	mux.HandleFunc("POST /snippet/create", postSnippetCreate)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.getSnippetView)
+	mux.HandleFunc("GET /snippet/create", app.getSnippetForm)
+	mux.HandleFunc("POST /snippet/create", app.postSnippetCreate)
 
 	logger.Info("starting a server", "addr", *addr)
 
 	err := http.ListenAndServe(*addr, mux)
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+type application struct {
+	logger *slog.Logger
 }
